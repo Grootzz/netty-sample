b2e2a2a1b16185fce64faf423795d5ab28374d7d
Grootzz <noodle.ax@gmail.com>
2019-06-23 10:28:18 +0800

    WebSocket通信

---
+++ netty-echo/src/main/java/com/sample/websocket/MyServer.java
@@ -0,0 +1,38 @@
+package com.sample.websocket;
+
+import com.sample.heartbeat.MyServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * WebSocket server
+ *
+ * @author noodle
+ * @date 2019/6/22 22:24
+ */
+public class MyServer {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO)) // boss event loop的日志处理器
+                    .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(3333).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ netty-echo/src/main/java/com/sample/websocket/WebSocketChannelInitializer.java
@@ -0,0 +1,28 @@
+package com.sample.websocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+
+/**
+ * @author noodle
+ * @date 2019/6/22 22:26
+ */
+public class WebSocketChannelInitializer extends ChannelInitializer<SocketChannel>{
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new HttpServerCodec()); // http 处理器
+        pipeline.addLast(new ChunkedWriteHandler()); // 以块写的处理器
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        pipeline.addLast(new TextWebSocketFrameHandler());
+    }
+}
